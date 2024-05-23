81be46d141e35a6a2b38a0dd2df7e8eff71356d2
tybruno <tybruno@cisco.com>
2024-05-23 12:04:23 -0600

    Adding `ModifiableItemsAttrDict`.

--- modifiable_items_dictionary/__init__.py
+++ modifiable_items_dictionary/__init__.py
@@ -1,3 +1,4 @@
 from modifiable_items_dictionary.modifiable_items_dictionary import ModifiableItemsDict
+from modifiable_items_dictionary.modifiable_items_attribute_dictionary import ModifiableItemsAttrDict
 
-__all__ = (ModifiableItemsDict.__name__,)
+__all__ = (ModifiableItemsDict.__name__, ModifiableItemsAttrDict.__name__)
